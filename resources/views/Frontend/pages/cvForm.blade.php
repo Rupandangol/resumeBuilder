7237731da4fe2ba9814cfb81c360a4a75660edea
rupan <rupandangol@Rupans-MacBook-Pro.local>
2019-07-12 18:00:26 +0545

    added date range picker

--- resources/views/Frontend/pages/cvForm.blade.php
+++ resources/views/Frontend/pages/cvForm.blade.php
@@ -8,10 +8,14 @@
 @section('content')
     <div class="box box-info">
 
+
+        {{--personal details--}}
+
         <div class="box-body">
             <div class="box-header with-border">
                 <h3 class="box-title">Personal Details</h3>
             </div>
+            <br>
             <div class="input-group">
                 <span class="input-group-addon">@</span>
                 <input type="text" class="form-control" placeholder="Full Name">
@@ -37,7 +41,9 @@
                 <input type="text" class="form-control" placeholder="Contact Address">
             </div>
             <br>
+            {{--end of Personal details--}}
 
+            {{--personal profiles--}}
 
             <div class="box-header with-border">
                 <h3 class="box-title">Personal Profile</h3>
@@ -62,7 +68,46 @@
                 <label>Career Summary</label>
                 <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
             </div>
+            {{--end of personal profile--}}
 
+            {{--academic qualification--}}
+
+            <div class="box-header with-border">
+                <h3 class="box-title">Education</h3>
+            </div>
+            <br>
+
+
+            <div id="education_field">
+
+            </div>
+
+            <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-tags"></i></span>
+                <input type="text" class="form-control" placeholder="School/Collage Name">
+            </div>
+            <br>
+            <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-map-pin"></i></span>
+                <input type="text" class="form-control" placeholder="School/Collage Location">
+            </div>
+            <br>
+            <div class="row">
+                <div class="form-group col-md-5">
+                    <label>Date range:</label>
+
+                    <div class="input-group">
+                        <div class="input-group-addon">
+                            <i class="fa fa-calendar"></i>
+                        </div>
+                        <input type="text" name="dateRange" class="form-control pull-right" id="reservation">
+                    </div>
+                </div>
+            </div>
+            <br>
+
+
+            {{--end of academic qualification--}}
 
             <div class="box-footer">
                 <button type="submit" class="btn btn-primary">Submit</button>
@@ -72,4 +117,22 @@
         </div>
         <!-- /.box-body -->
     </div>
+@endsection
+
+@section('my-footer')
+    <script type="text/javascript">
+        //Date range picker
+        $('#reservation').daterangepicker()
+    </script>
+    <script>
+        $(document).ready(function () {
+            var count = 1;
+            dynamic_experience(count);
+
+            function dynamic_experience(number) {
+
+            }
+
+        })
+    </script>
 @endsection
